51311dc378e707fa9b866bd62ae89b42c6da9e93
GeorgiosTE <gteleftheriou@gmail.com>
2023-05-07 16:56:27 +0300

    no message

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -67,7 +67,6 @@ public class GameManager : MonoBehaviour
 
             if(Physics.Raycast(ray, out hit))
             {
-                Debug.Log("raycast inside");
                 if(hit.collider.gameObject.CompareTag("SpaceObject"))
                 {
                     targetedPlanet = hit.collider.gameObject;
